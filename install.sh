2ed7cd1eb7dd091b6a9ff6aa300bc7b710ba5df7
HolyWalley <yasha.krasnov@gmail.com>
2022-03-19 11:52:21 +0000

    Install vim-plug for neovim

--- install.sh
+++ install.sh
@@ -12,6 +12,9 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
 mkdir ~/.config
 mkdir ~/.config/nvim
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 CURRENT_DIR=`pwd`
 
 ln -sf $CURRENT_DIR/zsh/.zshrc ~/.zshrc
