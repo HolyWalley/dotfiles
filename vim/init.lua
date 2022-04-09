335361d3d4c2e131f31487dc8ef59a68647a061e
HolyWalley <yasha.krasnov@gmail.com>
2022-04-09 17:55:48 +0200

    Add tailwindcss to language servers.

--- vim/init.lua
+++ vim/init.lua
@@ -215,7 +215,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'solargraph', 'denols', 'dockerls', 'yamlls' }
+local servers = { 'solargraph', 'denols', 'dockerls', 'yamlls', 'tailwindcss' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
