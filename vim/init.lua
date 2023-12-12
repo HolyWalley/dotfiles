14edcbdabc4ca9ba208143d39d4c2566a1dca103
HolyWalley <yakau@hey.com>
2023-12-12 23:11:38 +0100

    Update personal configs.

--- vim/init.lua
+++ vim/init.lua
@@ -1,4 +1,5 @@
 local vim = vim
+local copilot_enabled = true
 
 local execute = vim.api.nvim_command
 local fn = vim.fn
@@ -81,13 +82,25 @@ packer.startup(function()
   use 'akinsho/toggleterm.nvim'
   use 'editorconfig/editorconfig-vim'
 
-  use 'github/copilot.vim'
+  -- use 'github/copilot.vim'
+  use { "zbirenbaum/copilot.lua" }
 
   -- LetCode
   use { 'Dhanus3133/LeetBuddy.nvim', requires = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' } }
   -- Folding plugin, zc, zo, zr, zm
   use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
 end)
+
+-- Copilot
+require("copilot").setup({
+  suggestion = {
+    enabled = copilot_enabled,
+    auto_trigger = true,
+    keymap = {
+      accept = "<Tab>"
+    }
+  }
+})
 
 -- Do not check for perl provider
 vim.g['loaded_perl_provider'] = 0
@@ -234,31 +247,6 @@ map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
 map('n', 'gh', '<C-W>]', opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-end
-
--- vim.print(vim.lsp.buf)
-
 require('lspconfig').ruby_ls.setup {
   on_attach = function(client, bufnr)
     -- go to definition
@@ -274,20 +262,8 @@ require('lspconfig').ruby_ls.setup {
 }
 
 require'lspconfig'.stimulus_ls.setup{}
-
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { 'denols' }
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    -- flags = {
-    --   -- This will be the default in neovim 0.7+
-    --   debounce_text_changes = 150,
-    -- },
-    capabilities = capabilities
-  }
-end
+require'lspconfig'.tailwindcss.setup {}
+require'lspconfig'.standardrb.setup{}
 
 require'lspconfig'.eslint.setup{
   packageManager = 'yarn',
