f986a5b53186871adb5e653903d3c43ecd039fb4
HolyWalley <yakau@hey.com>
2024-03-16 09:34:53 +0100

    Set terminal window fix size.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,4 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Autocommand that sets winfixheight for terminal windows
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "*",
+	command = "setlocal winfixheight",
+})
+
 return {}
